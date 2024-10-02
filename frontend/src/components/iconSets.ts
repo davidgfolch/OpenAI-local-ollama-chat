49ae19257746c08e04ac22a66ec78fcfcef91cec
davidgfolch@gmail.com <davidgfolch@gmail.com>
2024-10-02 10:58:21 +0200

    wtf

---
+++ frontend/src/components/iconSets.ts
@@ -0,0 +1,10 @@
+const veloAI = {
+    user: "../assets/veloai/user.png",
+    ai: "../assets/veloai/ai.png",
+    loading: "../assets/veloai/loading.png",
+    trash: "../assets/veloai/trash.png",
+    settings: "../assets/veloai/settings.png",
+    send: "../assets/veloai/send.png"
+}
+const iconsSet=veloAI
+export default iconsSet;
