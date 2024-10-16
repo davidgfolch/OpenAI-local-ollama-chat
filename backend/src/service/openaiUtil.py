2a790892a3b87bdbd54b0f273d48b7c3e21bc2d6
davidgfolch@gmail.com <davidgfolch@gmail.com>
2024-10-16 17:31:21 +0200

    backend coverage

--- backend/src/service/openaiUtil.py
+++ backend/src/service/openaiUtil.py
@@ -11,7 +11,7 @@ openAICli = OpenAI(**hostArgs)
 
 def getModels() -> list:
     res: SyncPage[Model] = openAICli.models.list()
-    if res.data is None:
+    if not res.data:
         raise Exception("No available models found in Ollama, you need to: ollama pull <model>")
     res = list(map(lambda m: m.id, res.data))
     res.sort()
---
+++ backend/test/test_openaiUtil.py
@@ -0,0 +1,33 @@
+from typing import List
+import unittest
+from unittest.mock import patch, MagicMock
+from openai import OpenAI
+from openai.pagination import SyncPage
+from openai.types import Model
+from service.openaiUtil import getModels
+
+
+class TestGetModels(unittest.TestCase):
+
+    MagicMock(spec=OpenAI)
+
+    @staticmethod
+    def mock_SyncPage(mock, data: List[Model]):
+        mock.models.list.return_value = SyncPage[Model](object='', data=data)
+
+    @patch('service.openaiUtil.openAICli')
+    def test_getModels(self, mock_openAICli):
+        self.mock_SyncPage(mock_openAICli, [Model(id='model1', object='model', created=1, owned_by='x'),
+                                            Model(id='model2', object='model', created=1, owned_by='x')])
+        result = getModels()
+        self.assertEqual(result, ['model1', 'model2'])
+
+    @patch('service.openaiUtil.openAICli')
+    def test_getModels_no_data(self, mock_openAICli):
+        self.mock_SyncPage(mock_openAICli, [])
+        with self.assertRaises(Exception):
+            getModels()
+
+
+if __name__ == '__main__':
+    unittest.main()
