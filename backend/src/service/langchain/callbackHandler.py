e1aad187dff955eff2139dfee0b84cd7edcd8207
davidgfolch@gmail.com <davidgfolch@gmail.com>
2024-10-20 23:54:48 +0200

    decouple langchain classes from truncateStrings.py

--- backend/src/service/langchain/callbackHandler.py
+++ backend/src/service/langchain/callbackHandler.py
@@ -3,22 +3,32 @@ from typing import Any, Dict
 from langchain_core.callbacks import BaseCallbackHandler
 from langchain_core.outputs import LLMResult
 from langchain_core.messages import BaseMessage
+from langchain_core.prompt_values import ChatPromptValue
+from langchain_core.outputs.chat_generation import ChatGenerationChunk
 
-from service.langchain.truncateStrings import truncateStrings
+from service.langchain.truncateStrings import truncateStrings, setTruncateAttrMap
 from util.logUtil import initLog
 
 log = initLog(__file__, logging.DEBUG)
 
+def setTruncateLangchainClasses():
+    setTruncateAttrMap({
+        BaseMessage: ["content"],
+        ChatPromptValue: ["messages"],
+        LLMResult: ["generations"],
+        ChatGenerationChunk: ["text", "message"]
+    })
+
 
 class CallbackHandler(BaseCallbackHandler):
     def on_chat_model_start(self, serialized: dict[str, Any], messages: list[list[BaseMessage]], **kwargs) -> None:
-        log.debug(f"on_chat_model_start  serialized={truncateStrings(serialized)}, messages={truncateStrings(messages)}")
+        log.debug(f"on_chat_model_start ==> serialized={truncateStrings(serialized)}, messages={truncateStrings(messages)}")
 
     def on_llm_end(self, response: LLMResult, **kwargs) -> None:
-        log.debug(f"on_llm_end: response={truncateStrings(response)}")
+        log.debug(f"on_llm_end ==> response={truncateStrings(response)}")
 
     def on_chain_start(self, serialized: Dict[str, Any], inputs: Dict[str, Any], **kwargs) -> None:
-        log.debug(f"on_chain_start: serialized={truncateStrings(serialized)}, inputs={truncateStrings(inputs)}")
+        log.debug(f"on_chain_start ==> serialized={truncateStrings(serialized)}, inputs={truncateStrings(inputs)}")
 
     def on_chain_end(self, outputs: Dict[str, Any], **kwargs) -> None:
-        log.debug(f"on_chain_end: outputs={truncateStrings(outputs)}")
+        log.debug(f"on_chain_end ==> outputs={truncateStrings(outputs)}")
--- backend/src/service/langchain/truncateStrings.py
+++ backend/src/service/langchain/truncateStrings.py
@@ -1,52 +1,87 @@
 import collections
+import collections.abc
 import copy
 import logging
-from typing import Any
-from langchain_core.messages import BaseMessage
+from typing import Any, Dict
 
 from util.logUtil import initLog
 
-log = initLog(__file__, logging.ERROR)
+log = initLog(__file__, logging.INFO)
+
+__objAttributesMap: Dict[object, str] = {}
 
 
-def truncateString(str: str, max: int, suffix: str, deepStr: str) -> str:
-    mx = max - len(suffix)
-    if len(str) > mx:
-        truncated = str[:mx + len(suffix)] + suffix
-        return truncated
-    return str
+def setTruncateAttrMap(objAttributesMap: dict):
+    global __objAttributesMap
+    __objAttributesMap = objAttributesMap
+    log.info(f"setTruncateAttrMap objAttributesMap={__objAttributesMap}")
 
 
 def truncateStrings(o: Any, max: int = 40) -> str:
-    return truncateStringsInternal(copy.deepcopy(o), max)
+    """ Truncates any object strings recursively supporting dict, tuple, iterable out-of-the-box.
+
+    To parse special objects pass specific map with object properties you want to truncate, f.ex.:
+    ```
+    setTruncateAttrMap({
+        BaseMessage: ["content"],  # parses only content attribute (str) from BaseMessage
+        ChatPromptValue: ["messages"]  # parses only messages attribute (list) from ChatPrompValue
+    })
+    ```
+    """
+    return __truncateStrings(copy.deepcopy(o), max)
 
 
-def truncateStringsInternal(o: Any, max: int, suffix: str = "[...]", deep: int = 0) -> str:
+def __truncateStrings(o: Any, max: int, suffix: str = "[...]", deep: int = 0) -> str:
     if not o or isinstance(o, int) or isinstance(o, float) or isinstance(o, bool):
         return o
     try:
-        deepStr = ("   "*deep)
+        for classType in __objAttributesMap:
+            if isinstance(o, classType):
+                return __truncateClass(o, classType, max, suffix, deep+1)
         if isinstance(o, dict):
-            log.info(f"obj type = {type(o)} -> isinstance of dict")
-            for key_ in o.keys():
-                if key_:
-                    o[key_] = truncateStringsInternal(o[key_], max, suffix, deep+1)
-        elif isinstance(o, str):
-            log.info(f"obj type = {type(o)} -> isinstance of str")
-            o = truncateString(o, max, suffix, deepStr)
-        elif isinstance(o, BaseMessage):
-            log.info(f"obj type = {type(o)} -> isinstance of BaseMessage")
-            o.content = truncateStringsInternal(o.content, max, suffix, deep+1)
-        elif isinstance(o, tuple):
-            log.info(f"obj type = {type(o)} -> isinstance of tuple")
-            t: tuple = o
-            o = (t[0], truncateStringsInternal(t[1], max, suffix, deep+1))
-        elif isinstance(o, collections.abc.Iterable):
-            log.info(f"obj type = {type(o)} -> isinstance of Iterable")
-            for idx, item in enumerate(o):
-                o[idx] = truncateStringsInternal(item, max, suffix, deep+1)
-        else:
-            log.error(f"obj type = {type(o)} NOT IMPLEMENTED!!!!!!!!!!!!!!!!!!1")
+            return __truncateDict(o, max, suffix, deep+1)
+        if isinstance(o, str):
+            log.debug(f"obj type = {type(o)} -> isinstance of str")
+            return truncateString(o, max, suffix, deep+1)
+        if isinstance(o, tuple):
+            log.debug(f"obj type = {type(o)} -> isinstance of tuple")
+            return (o[0], __truncateStrings(o[1], max, suffix, deep+1))
+        if isinstance(o, collections.abc.Iterable):
+            return __truncateIterable(o, max, suffix, deep+1)
+        log.error(f"obj type = {type(o)} NOT IMPLEMENTED!!!!!!!!!!!!!!!!!!")
     except Exception as e:
-        log.error(e)
+        log.error(f"failed to truncate strings for object type {type(o)}", e)
     return o
+
+
+def __truncateClass(o: Any, classType: object, max: int, suffix: str, deep: int):
+    attributes = __objAttributesMap.get(classType)
+    if attributes:
+        for attr in attributes:
+            res = __truncateStrings(getattr(o, attr), max, suffix, deep+1)
+            setattr(o, attr, res)
+    return o
+
+
+def __truncateIterable(o: collections.abc.Iterable, max: int, suffix: str, deep: int):
+    log.debug(f"obj type = {type(o)} -> isinstance of Iterable")
+    for idx, item in enumerate(o):
+        o[idx] = __truncateStrings(item, max, suffix, deep+1)
+    return o
+
+
+def __truncateDict(o: Dict, max: int, suffix: str, deep: int):
+    log.debug(f"obj type = {type(o)} -> isinstance of dict")
+    for key_ in o.keys():
+        if key_:
+            o[key_] = __truncateStrings(o[key_], max, suffix, deep+1)
+    return o
+
+
+def truncateString(str: str, max: int, suffix: str, deep: int) -> str:
+    mx = max - len(suffix)
+    if len(str) > mx:
+        truncated = str[:mx + len(suffix)] + suffix
+        log.debug(("   "*deep)+f"truncating string to: {truncated}")
+        return truncated
+    return str
