c2bd2019901d3dcf77a5f9da5cf2a730fcc4a2da
davidgfolch@gmail.com <davidgfolch@gmail.com>
2024-10-22 15:28:38 +0200

    dynamic background image

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,29 @@
-<script>
+<script setup>
+import { onMounted } from 'vue';
 import ChatHeader from './components/ChatHeader.vue';
 import ChatContainer from './components/ChatContainer.vue';
 
-export default {
-  components: {
-    ChatHeader,
-    ChatContainer
-  }
+const setBackgroundImage = () => {
+	document.body.style = "background: url('/background" + (Math.floor(Math.random() * 4) + 1) + ".png'); 	background-repeat: no-repeat; background-size: cover; background-attachment: fixed;";
+	setTimeout(setBackgroundImage, 60000);
 }
+onMounted(setBackgroundImage);
 </script>
 
 <template>
-  <ChatHeader msg="OpenAi local Ollama chat" />
-  <ChatContainer/>
+	<ChatHeader msg="OpenAi local Ollama chat" />
+	<ChatContainer />
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Lato');
+
 * {
 	box-sizing: border-box;
 }
 
 body {
-	background: url('assets/background1.png');
-  background-repeat: no-repeat;
-	background-size: cover;
-  background-attachment: fixed;
+	background-color: black;
 	display: flex;
 	align-items: center;
 	justify-content: center;
